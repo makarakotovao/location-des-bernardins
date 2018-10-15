ae16e5d6d24da67425e85aa6663a5fc8c05573e7
AureliaG <garnier.aurelia.emeline@gmail.com>
2018-10-15 15:06:54 +0200

    fixage bug menu: update plugin srollTo

--- contact.php
+++ contact.php
@@ -388,9 +388,9 @@ if (!empty($_POST['cdbmail']))
 
 		<script type="text/javascript" src="js-plugin/magnific-popup/jquery.magnific-popup.min.js"></script>
 
-		<script type="text/javascript" src="js-plugin/parallax/js/jquery.scrollTo-1.4.3.1-min.js"></script>
+		<script type="text/javascript" src="js-plugin/parallax/js/jquery.scrollTo.2.0.0-min.js"></script>
 
-		<script type="text/javascript" src="js-plugin/parallax/js/jquery.localscroll-1.2.7-min.js"></script>
+		<script type="text/javascript" src="js-plugin/parallax/js/jquery.localscroll.2.0.0-min.js"></script>
 
 		<script type="text/javascript" src="js-plugin/parallax/js/jquery.stellar.min.js"></script>
 
